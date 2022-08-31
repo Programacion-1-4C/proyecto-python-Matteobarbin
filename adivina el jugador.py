e4a0a56f423b7b1ae7d8e62ef2d12cbbd4412c1e
Mateo Barbini <mattebarbini@gmail.com>
2022-08-31 09:51:42 -0300

    le agregue una funcion de busqueda

--- adivina el jugador.py
+++ adivina el jugador.py
@@ -1,23 +1,29 @@
 from funcionmenu import adivina_el_jugadort
+contraseña =123
+contra=int(input("ingrese la contraseña>>>"))
+if contra==contraseña:
+    print("contraseña correcta")
+    jugadorest = "perez,diaz,villagra,valoyes,herrera,aguerre,benavidez,catalan,martino,malatini,franco,garro,esquivel,godoy,ortegoza,bufarini,suarez,girotti,santos,romero,fertoli "
+    jugadorest = jugadorest.split(",")
+    final_jugadorest = []
+    for x in jugadorest:
+        jugadorest = x.strip()
+        final_jugadorest.append(jugadorest)
+    puntos = 0
+    while True:
+        print("1.ingrese un nuevo jugador")
+        print("2.vamo a jugar")
+        operacion = int(input(""))
+        if operacion == 1:
+            nuevo_jugadorest = str(input("ingrese un nuevo jugador"))
 
-jugadorest = "perez,diaz,villagra,valoyes,herrera,aguerre,benavidez,catalan,martino,malatini,franco,garro,esquivel,godoy,ortegoza,bufarini,suarez,girotti,santos,romero,fertoli "
-jugadorest = jugadorest.split(",")
-final_jugadorest = []
-for x in jugadorest:
-    jugadorest = x.strip()
-    final_jugadorest.append(jugadorest)
-puntos = 0
-while True:
-    print("1.ingrese un nuevo jugador")
-    print("2.vamo a jugar")
-    operacion = int(input(""))
-    if operacion == 1:
-        nuevo_jugadorest = str(input("ingrese un nuevo jugador"))
+            if nuevo_jugadorest not in final_jugadorest:
+                final_jugadorest.append(nuevo_jugadorest)
+            else:
+                print("el jugador ya esta en la lista")
+        if operacion == 2:
+            adivina_el_jugadort(puntos, final_jugadorest)
 
-        if nuevo_jugadorest not in final_jugadorest :
-            final_jugadorest.append(nuevo_jugadorest)
-        else:
-            print("el jugador ya esta en la lista")
-    if operacion == 2:
-        adivina_el_jugadort(puntos, final_jugadorest)
+else :
+  print("contraseña incorrecta")
 
